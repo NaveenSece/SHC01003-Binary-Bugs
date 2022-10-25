ab96e221da9c833a25722415475986657f40721c
NaveenSece <116641424+NaveenSece@users.noreply.github.com>
2022-10-25 17:44:56 +0530

    Add files via upload

---
+++ SHC01003-binaryBugs/src/hackathon/Hackathon.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hackathon;
+
+/**
+ *
+ * @author horne
+ */
+public class Hackathon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IntroPage i = new IntroPage();
+        i.setVisible(true);
+    } 
+}
